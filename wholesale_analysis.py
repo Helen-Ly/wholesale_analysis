295df113b26fbb1401f45a2e9204d753ce75d91e
Helen-Ly <helenly25@gmail.com>
2020-11-22 18:11:16 -0500

    Pseudocode steps.

---
+++ wholesale_analysis.py
@@ -0,0 +1,22 @@
+# Import dependencies
+
+# Open and pull data 
+
+######################################
+# Count  total vehicles sold within the 2 year time frame
+# Get list of unique buyers
+# Determine how many times each buyer purchased from us
+# Determine percentage for buyers
+# Determine top buyer
+######################################
+
+######################################
+# Determine unique vehicles sold
+# Determine how many were sold for each model
+# Determine percentage told
+# Determine top sold vehicle
+######################################
+
+######################################
+# Write out results to text file
+######################################
