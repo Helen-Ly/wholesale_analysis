61eb3a1ee82679f0263d65dfb19e0aa5485a6a1d
Helen-Ly <helenly25@gmail.com>
2020-11-22 22:28:55 -0500

    Upload buyer list and dictionary.

--- wholesale_analysis.py
+++ wholesale_analysis.py
@@ -13,27 +13,55 @@ file_to_load = os.path.join("Resources", "UCD - WHOLESALE.csv")
 # Determine top buyer
 ######################################
 
+# Create variable for total entry
+total_entries = 0
+
+# Create list for unique buyers
+buyer_name = []
+
+# Create dictionary for each buyer
+buyer_dictionary = {}
 
 with open(file_to_load, "r") as wholesale_data:
 
-    # Create variable for total entry
-    total_entries = 0
+    # Read the dat
+    file_reader = csv.reader(wholesale_data)
 
-    # Show header
-    header = next(wholesale_data)
+    # Show header 
+    header = next(file_reader)
     print(header)
 
     # Create a for loop to count each entry
-    for entry in wholesale_data:
-
-        # Variable for buyer
-        buyer = entry[2]
+    for row in file_reader:
 
         # Add count to total_entries
         total_entries += 1
+
+        # Variable to pull out buyer name
+        buyer = row[2]
+
+        # Append to the buyer_name list
+        if buyer not in buyer_name:
+
+            # Append buyer
+            buyer_name.append(buyer)
+
+            # Initialize buyer dictionary
+            buyer_dictionary[buyer] = 0
+
+        # Add count for buyer in dictionary
+        buyer_dictionary[buyer] += 1
+
+            
     
     # Print out total entries
     print(total_entries)
+
+    # Print out buyer_name list
+    print(buyer_name)
+
+    # Print out buyer_dictionary
+    print(buyer_dictionary)
 
 
 
